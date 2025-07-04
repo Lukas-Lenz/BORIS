befc282e83394d5fc7d36111bc711449595db9a0
Lukas <LenzLukas@proton.me>
2025-07-04 16:21:25 +0200

    dropdownlists

--- BORIS/Models/Locations.cs
+++ BORIS/Models/Locations.cs
@@ -1,6 +1,14 @@
-﻿namespace BORIS.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BORIS.Models
 {
-    public class Locations
+    public class LocationList
     {
+        public static List<SelectListItem> SelectionItems =
+            [
+                new SelectListItem { Text = "Keine Auswahl", Value = "NONE" },
+                new SelectListItem { Text = "Neuss", Value = "NEUSS" },
+                new SelectListItem { Text = "Bochum", Value = "BOCHUM" }
+            ];
     }
 }
